c687979a57e9da9350237e0b3cd9067cb581971d
unknown <juan.aguas@labinfo.is.escuelaing.edu.co>
2022-08-12 13:10:10 -0500

    Puerto añadido

--- src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
@@ -4,8 +4,9 @@ import static spark.Spark.*;
 
 public class SparkWebApp {
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
-        }
+    }
 
     static int getPort() {
         if (System.getenv("PORT") != null) {
